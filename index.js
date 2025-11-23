3fc410c2d08aa2c6af3e9559452009bf853585a0
rishabh <rishabhkushwah4040@gmail.com>
2025-11-23 17:45:27 +0530

    Add node server files

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-console.log("hello world!")
+const express = require("express");
+const app = express();
+
+app.use(express.json());
+
+// Test route
+app.get("/", (req, res) => {
+  res.send("Server is running...");
+});
+
+// Start server
+const PORT = 3000;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
